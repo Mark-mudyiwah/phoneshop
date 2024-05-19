20e6b403d75338bb063f5304a1612f89b68cd51d
mark <markmudyiwah0@gmail.com>
2024-05-19 19:20:58 +0200

    fixed  the products html and change images location

---
+++ data/products.js
@@ -0,0 +1,362 @@
+const products = [
+
+    {
+        image:'images/img/Apple MacBook.webp',
+        name:' Apple MacBook',
+        price: 22000
+    
+      },
+      {
+        image: 'images/img/Dell Laptop.webp',
+        name: 'Dell Laptop',
+        price: 11000
+    
+      },
+    
+      {
+        image: 'images/img/Asus Laptop.webp',
+        name:  'Asus VivoBook',
+        price: 8999.9
+         
+      },
+      {
+        image:'images/img/Apple MacBook.webp',
+        name:' Apple MacBook',
+        price: 22000
+    
+      },
+      {
+        image: 'images/img/Dell Laptop.webp',
+        name: 'Dell Laptop',
+        price: 11000
+    
+      },
+    
+      {
+        image: 'images/img/Asus Laptop.webp',
+        name:  'Asus VivoBook',
+        price: 8999.9
+    
+      },
+      {
+        image:'images/img/Apple MacBook.webp',
+        name:' Apple MacBook',
+        price: 22000
+    
+      },
+      {
+        image: 'images/img/Dell Laptop.webp',
+        name: 'Dell Laptop',
+        price: 11000
+    
+      },
+    
+      {
+        image: 'images/img/Asus Laptop.webp',
+        name:  'Asus VivoBook',
+        price: 8999.9
+    
+      },
+      {
+        image:'images/img/Apple MacBook.webp',
+        name:' Apple MacBook',
+        price: 22000
+    
+      },
+      {
+        image: 'images/img/Dell Laptop.webp',
+        name: 'Dell Laptop',
+        price: 11000
+    
+      },
+    
+      {
+        image: 'images/img/Asus Laptop.webp',
+        name:  'Asus VivoBook',
+        price: 8999.9
+    
+      },
+{
+    image:'images/img/Apple MacBook.webp',
+    name:' Apple MacBook',
+    price: 22000
+
+  },
+  {
+    image: 'images/img/Dell Laptop.webp',
+    name: 'Dell Laptop',
+    price: 11000
+
+  },
+
+  {
+    image: 'images/img/Asus Laptop.webp',
+    name:  'Asus VivoBook',
+    price: 8999.9
+
+  },
+
+
+  {
+    image:'images/img/Apple MacBook.webp',
+    name:' Apple MacBook',
+    price: 22000
+
+  },
+  {
+    image: 'images/img/Dell Laptop.webp',
+    name: 'Dell Laptop',
+    price: 11000
+
+  },
+
+  {
+    image: 'images/img/Asus Laptop.webp',
+    name:  'Asus VivoBook',
+    price: 8999.9
+
+  },
+  {
+    image:'images/img/Apple MacBook.webp',
+    name:' Apple MacBook',
+    price: 22000
+
+  },
+  {
+    image: 'images/img/Dell Laptop.webp',
+    name: 'Dell Laptop',
+    price: 11000
+
+  },
+
+  {
+    image: 'images/img/Asus Laptop.webp',
+    name:  'Asus VivoBook',
+    price: 8999.9
+
+  },
+  {
+    image:'images/img/Apple MacBook.webp',
+    name:' Apple MacBook',
+    price: 22000
+
+  },
+  {
+    image: 'images/img/Dell Laptop.webp',
+    name: 'Dell Laptop',
+    price: 11000
+
+  },
+
+  {
+    image: 'images/img/Asus Laptop.webp',
+    name:  'Asus VivoBook',
+    price: 8999.9
+
+  },
+  {
+    image:'images/img/Apple MacBook.webp',
+    name:' Apple MacBook',
+    price: 22000
+
+  },
+  {
+    image: 'images/img/Dell Laptop.webp',
+    name: 'Dell Laptop',
+    price: 11000
+
+  },
+
+  {
+    image: 'images/img/Asus Laptop.webp',
+    name:  'Asus VivoBook',
+    price: 8999.9
+
+  },
+  {
+    image:'images/img/Apple MacBook.webp',
+    name:' Apple MacBook',
+    price: 22000
+
+  },
+  {
+    image: 'images/img/Dell Laptop.webp',
+    name: 'Dell Laptop',
+    price: 11000
+
+  },
+
+  {
+    image: 'images/img/Asus Laptop.webp',
+    name:  'Asus VivoBook',
+    price: 8999.9
+
+  },
+  {
+    image:'images/img/Apple MacBook.webp',
+    name:' Apple MacBook',
+    price: 22000
+
+  },
+  {
+    image: 'images/img/Dell Laptop.webp',
+    name: 'Dell Laptop',
+    price: 11000
+
+  },
+
+  {
+    image: 'images/img/Asus Laptop.webp',
+    name:  'Asus VivoBook',
+    price: 8999.9
+
+  },
+  {
+    image:'images/img/Apple MacBook.webp',
+    name:' Apple MacBook',
+    price: 22000
+
+  },
+  {
+    image: 'images/img/Dell Laptop.webp',
+    name: 'Dell Laptop',
+    price: 11000
+
+  },
+
+  {
+    image: 'images/img/Asus Laptop.webp',
+    name:  'Asus VivoBook',
+    price: 8999.9
+
+  },
+  {
+    image:'images/img/Apple MacBook.webp',
+    name:' Apple MacBook',
+    price: 22000
+
+  },
+  {
+    image: 'images/img/Dell Laptop.webp',
+    name: 'Dell Laptop',
+    price: 11000
+
+  },
+
+  {
+    image: 'images/img/Asus Laptop.webp',
+    name:  'Asus VivoBook',
+    price: 8999.9
+
+  },
+  {
+    image:'images/img/Apple MacBook.webp',
+    name:' Apple MacBook',
+    price: 22000
+
+  },
+  {
+    image: 'images/img/Dell Laptop.webp',
+    name: 'Dell Laptop',
+    price: 11000
+
+  },
+
+  {
+    image: 'images/img/Asus Laptop.webp',
+    name:  'Asus VivoBook',
+    price: 8999.9
+
+  },
+  {
+    image:'images/img/Apple MacBook.webp',
+    name:' Apple MacBook',
+    price: 22000
+
+  },
+  {
+    image: 'images/img/Dell Laptop.webp',
+    name: 'Dell Laptop',
+    price: 11000
+
+  },
+
+  {
+    image: 'images/img/Asus Laptop.webp',
+    name:  'Asus VivoBook',
+    price: 8999.9
+
+  },
+  {
+    image:'images/img/Apple MacBook.webp',
+    name:' Apple MacBook',
+    price: 22000
+
+  },
+  {
+    image: 'images/img/Dell Laptop.webp',
+    name: 'Dell Laptop',
+    price: 11000
+
+  },
+
+  {
+    image: 'images/img/Asus Laptop.webp',
+    name:  'Asus VivoBook',
+    price: 8999.9
+
+  },
+  {
+    image:'images/img/Apple MacBook.webp',
+    name:' Apple MacBook',
+    price: 22000
+
+  },
+  {
+    image: 'images/img/Dell Laptop.webp',
+    name: 'Dell Laptop',
+    price: 11000
+
+  },
+
+  {
+    image: 'images/img/Asus Laptop.webp',
+    name:  'Asus VivoBook',
+    price: 8999.9
+
+  },
+
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,19 +1,19 @@
 const products =[
   {
-    image:'images/img/Apple MacBook.webp',
+    image:'images/products/Laptops&monitors/Apple MacBook.webp',
     name:' Apple MacBook',
     price: 22000
 
   },
   {
-    image: 'images/img/Dell Laptop.webp',
+    image: 'images/products/Laptops&monitors/Dell Laptop.webp',
     name: 'Dell Laptop',
     price: 11000
 
   },
 
   {
-    image: 'images/img/Asus Laptop.webp',
+    image: 'images/products/Laptops&monitors/Asus Laptop.webp',
     name:  'Asus VivoBook',
     price: 8999.9
 
@@ -32,8 +32,42 @@ products.forEach((product)=>{
     <div class="product-name-container">
     ${product.name}
     </div>
+
+    <div class ="product-specs">
+
+    <div class = "storage-rom-container"> 
+    <div class="rom-text-container">
+    ROM :
+    </div>
+    <div class="rom-amount">
+      ${256}GB
+    </div>
+    </div>
+    <div class = "storage-ram-container">
+    <div class="ram-text-container">
+     RAM :
+    </div>
+     <div class="ram-amount">
+     25GB
+     </div>
+    </div>
+    <div class = "product-color-container">
+    
+     <div class="color-text-container">
+     Color :
+     </div>
+     <div class="product-color">
+      ${'Blue'}
+     </div>
+    </div>
+    </div>
     <div class =  "product-price-container">
-     R ${(product.price).toFixed(2)}
+    <div class="product-price-text">
+    Price :
+    </div>
+    <div class = "amount-container">
+    R${(product.price).toFixed(2)}
+    </div>
     </div>
     <div class="added-text-div">
  
