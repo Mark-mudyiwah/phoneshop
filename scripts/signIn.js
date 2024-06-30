b6199b89a7f5f5685aa21a20f4668ded528581da
mark <markmudyiwah0@gmail.com>
2024-06-30 04:33:03 +0200

    create new files for sign in

---
+++ scripts/signIn.js
@@ -0,0 +1,22 @@
+
+let clientInfo = [
+
+{ firstName:'Mark',
+  surName  :'Mudyiwa',
+  dateOfBirth:'2024/04/24',
+  gender:'male',
+  userName:'markmudyiwah0@gmail.com',
+  
+
+
+
+},
+
+{
+
+
+
+
+}
+
+]
